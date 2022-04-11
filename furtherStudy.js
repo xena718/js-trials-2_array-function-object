d51c76847699869f19ac7509ba5bb791d28019d0
Chengfeng Ren <chengfengren@gmail.com>
2022-04-11 23:19:07 +0000

    update further study

--- furtherStudy.js
+++ furtherStudy.js
@@ -1,9 +1,45 @@
 'use strict';
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const words1Set = new Set(words1);
+  const words2Set = new Set(words2);
+  const result = new Set();
+  for (const word of words1Set){
+    if (words2Set.has(word)){
+      result.add(word);
+    }
+  }
+  return Array.from(result);
 }
 
 function kidsGame(names) {
-  // Replace this with your code
+  const output = [names.shift()];
+
+  firstLetterToWords = {};
+
+  for (const name of names) {
+    const firstChar = name[0];
+    if (!firstLetterToWords[firstChar]) {
+      firstLetterToWords[firstChar] = [name];
+    } else {
+      firstLetterToWords[firstChar].push(name);
+    }
+  }
+
+  while (true) {
+    const lastWord = output[output.length - 1];
+    const lastWordChar = lastWord[lastWord.length - 1];
+
+    if (
+      firstLetterToWords[lastWordChar] === undefined ||
+      firstLetterToWords[lastWordChar].length === 0
+    ) {
+      break;
+    }
+
+    const nextWord = firstLetterToWords[lastWordChar].shift();
+    output.push(nextWord);
+  }
+
+  return output;
 }
