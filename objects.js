ee512d36161d764e2c580806ad97e20b94627c27
Chengfeng Ren <chengfengren@gmail.com>
2022-04-11 23:03:06 +0000

    completed

--- objects.js
+++ objects.js
@@ -2,10 +2,27 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const wordCount = {};
+  const words = phrase.split(' ');
+  for (const word of words){
+    if (words.includes(word)){
+      wordCount.word +=1;
+    }else{
+      wordCount.word =1;
+    }
+  }
+  return wordCount
 }
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melonPrices = {
+    '2.50': ['Cantaloupe', 'Honeydew'],
+    '2.95': ['Watermelon'],
+    '3.25': ['Musk', 'Crenshaw'],
+    '14.25': ['Christmas']};
+    if (melonPrices.hasOwnProperty('price')){
+      return melonPrices[price].sort();
+    }
+    return null;
 }
